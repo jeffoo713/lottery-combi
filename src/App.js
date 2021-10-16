81b8b4fa95cd1b5201785798a3d83c0f69013363
jeff <jeffk713@gmail.com>
2021-10-16 00:40:45 -0700

    create main page w/o style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='main'>
+        <div className='title'>
+          <h1>LOTTERY COMBINATION</h1>
+        </div>
+        <div className='input-container'>
+          <h2>Except Theses Numbers</h2>
+          <form className='form'>
+            <input className='input' type='number' name='num1' />
+            <input className='input' type='number' name='num2' />
+            <input className='input' type='number' name='num3' />
+            <input className='input' type='number' name='num4' />
+            <input className='input' type='number' name='num5' />
+            <input className='input' type='number' name='num6' />
+            <input type='submit' />
+          </form>
+        </div>
+        <div className='result-container'>
+          <h2 className='result1'>24</h2>
+          <h2 className='result1'>33</h2>
+          <h2 className='result1'>12</h2>
+          <h2 className='result1'>5</h2>
+          <h2 className='result1'>29</h2>
+          <h2 className='result1'>44</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
